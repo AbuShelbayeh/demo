385d6148e9a8eb3a3e47c7adc4280a14ce12e3e0
Saleem Abu Shelbayeh <saleem.abu.shelbaya@gmail.com>
2023-08-06 16:54:53 +0300

    Intro to Form Validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,15 +1,29 @@
 <?php
 
-
 $config = require 'config.php';
 $db = new Database($config['database'], 'saleem', 'password');
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(note,user_id) VALUES(:note, :user_id)',  [
-        'note' => $_POST['note'],
-        'user_id' => 1
-    ]);
+
+    $errors = [];
+
+    if (strlen($_POST['note']) === 0) {
+        $errors['note'] = 'A note is required';
+    }
+
+    if (strlen($_POST['note']) > 1000) {
+        $errors['note'] = "The note can't be more than 1000 chars";
+    }
+
+    if(empty($errors)) {
+
+        $db->query('INSERT INTO notes(note,user_id) VALUES(:note, :user_id)',  [
+            'note' => $_POST['note'],
+            'user_id' => 1
+        ]);
+
+    }
 }
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -15,7 +15,13 @@
                                     <div class="mt-1">
                                     <textarea id="note" name="note" rows="3"
                                               class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                              placeholder="Here's an idea for a note..."></textarea>
+                                              placeholder="Here's an idea for a note..."
+
+                                ><?= $_POST['note'] ?? '' ?></textarea>
+
+                                        <?php if(isset($errors['note'])) : ?>
+                                            <p class="text-red-500 text-xs mt-2"><?= $errors['note'] ?></p>
+                                        <?php endif; ?>
                                     </div>
                                 </div>
                             </div>
